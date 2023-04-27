94123f066d86f7d0ef370bff6a1eff4f664ad6f0
Jeff Walker <jeff@walkerenvres.com>
2023-04-26 20:40:36 -0400

    db funcs for full day run

--- db/functions.sql
+++ db/functions.sql
@@ -216,6 +216,218 @@ AS $$
   ORDER BY d.date
 $$ LANGUAGE SQL;
 
+
+-- run estimates by date, period (full day)
+CREATE OR REPLACE FUNCTION f_run_periods_full(_location_id text, _start_date text, _end_date text)
+RETURNS TABLE (
+  location_id TEXT,
+  date DATE,
+  period INT,
+  n REAL,
+  n_count REAL,
+  t REAL,
+  sum_y REAL,
+  sum_t REAL,
+  mean_t REAL,
+  r REAL,
+  y REAL,
+  se2 REAL,
+  var_y REAL,
+  df REAL,
+  a REAL
+)
+AS $$
+  WITH v AS (
+    SELECT
+      id,
+      location_id,
+      start_timestamp,
+      duration,
+      date_part('hour', start_timestamp AT TIME ZONE 'US/Eastern') AS start_hour,
+      date_part('hour', end_timestamp AT TIME ZONE 'US/Eastern') AS end_hour,
+      date_part('minute', end_timestamp AT TIME ZONE 'US/Eastern') AS end_minute
+    FROM videos
+    WHERE NOT flagged
+    AND location_id = _location_id
+    AND (start_timestamp AT TIME ZONE 'US/Eastern')::date >= _start_date::date
+    AND (end_timestamp AT TIME ZONE 'US/Eastern')::date <= _end_date::date
+  ),
+  c AS (
+    SELECT
+      c.video_id,
+      count(c.count) as n_count,
+      avg(c.count) as mean_count
+    FROM counts c
+    WHERE NOT c.flagged
+    AND c.video_id IN (SELECT id FROM v)
+    GROUP BY video_id
+  ),
+  vpd1 AS (
+    SELECT
+      v.id,
+      v.location_id,
+      v.start_timestamp AT TIME ZONE 'US/Eastern' as start_timestamp,
+      (v.start_timestamp AT TIME ZONE 'US/Eastern')::date AS date,
+      v.duration,
+      CASE
+        WHEN start_hour >= 0  AND start_hour < 4 THEN 1
+        WHEN start_hour >= 4  AND start_hour < 7 THEN 2
+        WHEN start_hour >= 7  AND start_hour < 11 THEN 3
+        WHEN start_hour >= 11 AND start_hour < 15 THEN 4
+        WHEN start_hour >= 15 AND start_hour < 19 THEN 5
+        WHEN start_hour >= 19 THEN 6
+        ELSE NULL
+      END AS period,
+      COALESCE(c.n_count, 0) as n_count,
+      c.mean_count as mean_count
+    FROM v
+    LEFT JOIN c ON v.id=c.video_id
+    WHERE NOT (v.end_hour != v.start_hour AND v.end_hour IN (0, 4, 7, 11, 15, 19) AND v.end_minute > 0)
+    ORDER BY v.start_timestamp
+  ),
+  pd_r AS (
+    SELECT
+      location_id,
+      date,
+      period,
+      sum(mean_count) / sum(duration * (n_count > 0)::int) AS r
+    FROM vpd1
+    GROUP BY location_id, date, period
+  ),
+  vpd2 AS (
+    SELECT
+      vpd1.*,
+      pd_r.r
+    FROM vpd1
+    LEFT JOIN pd_r ON vpd1.date=pd_r.date AND vpd1.period=pd_r.period AND vpd1.location_id=pd_r.location_id
+  ),
+  pd1 AS (
+    SELECT
+      location_id,
+      date,
+      period,
+      count(*)::real AS n,
+      sum((n_count > 0)::int)::real AS n_count,
+      sum(duration)::real AS t,
+      COALESCE(sum(mean_count), 0)::real AS sum_y,
+      sum(duration * (n_count > 0)::int) AS sum_t,
+      CASE
+        WHEN sum((n_count > 0)::int) > 0 THEN sum(duration * (n_count > 0)::int) / sum((n_count > 0)::int)
+        ELSE 0
+      END AS mean_t,
+      CASE
+        WHEN sum(duration * (n_count > 0)::int) > 0 THEN sum(mean_count) / sum(duration * (n_count > 0)::int)
+        ELSE 0
+      END AS r,
+      CASE
+        WHEN sum(duration * (n_count > 0)::int) > 0 THEN sum(mean_count) / sum(duration * (n_count > 0)::int) * sum(duration)
+        ELSE 0
+      END AS y,
+      CASE
+        WHEN (sum((n_count > 0)::int) - 1) > 0 THEN sum((mean_count - r * duration) ^ 2) / (sum((n_count > 0)::int) - 1)
+        ELSE 0
+      END AS se2
+    FROM vpd2
+    GROUP BY location_id, date, period
+  )
+  SELECT
+    location_id,
+    date,
+    period,
+    n::REAL,
+    n_count::REAL,
+    t::REAL,
+    sum_y::REAL,
+    sum_t::REAL,
+    mean_t::REAL,
+    r::REAL,
+    y::REAL,
+    se2::REAL,
+    (CASE
+    WHEN (pd1.n * pd1.n_count) > 0 THEN ((pd1.t / pd1.mean_t) ^ 2 * (pd1.n - pd1.n_count) / (pd1.n * pd1.n_count) * pd1.se2)
+    ELSE 0
+    END)::REAL AS var_y,
+    (pd1.n_count - 1)::REAL AS df,
+    (CASE
+      WHEN pd1.n_count > 0 THEN pd1.n * (pd1.n - pd1.n_count)::real / pd1.n_count
+      ELSE 0
+    END)::REAL AS a
+  FROM pd1
+  ORDER BY location_id, date, period
+$$ LANGUAGE SQL;
+
+-- run estimates by date (full day)
+CREATE OR REPLACE FUNCTION f_run_daily_full(_location_id text, _start_date text, _end_date text)
+RETURNS TABLE (
+  location_id TEXT,
+  date TEXT,
+  n REAL,
+  n_count REAL,
+  t REAL,
+  sum_y REAL,
+  sum_t REAL,
+  y REAL,
+  var_y REAL,
+  df_num REAL,
+  df_den REAL,
+  se_y REAL,
+  df INT
+)
+AS $$
+  WITH d1 AS (
+    SELECT
+      location_id,
+      date,
+      sum(n) AS n,
+      sum(n_count) AS n_count,
+      sum(t) AS t,
+      sum(sum_y) AS sum_y,
+      sum(sum_t) AS sum_t,
+      sum(y) AS y,
+      sum(var_y) AS var_y,
+      sum(a * se2) AS df_num,
+      sum(CASE WHEN n_count > 1 THEN (a * se2) ^ 2 / (n_count - 1) ELSE 0 END) AS df_den
+    FROM f_run_periods_full(_location_id, _start_date, _end_date)
+    GROUP BY location_id, date
+  ), d2 AS (
+    SELECT
+      location_id,
+      date,
+      n,
+      n_count,
+      t,
+      sum_y,
+      sum_t,
+      y,
+      var_y,
+      df_num::REAL,
+      df_den::REAL,
+      sqrt(var_y)::REAL AS se_y,
+      ROUND(CASE WHEN df_den > 0 THEN df_num ^ 2 / df_den ELSE 0 END)::INT AS df
+    FROM d1
+    ORDER BY location_id, date
+  ), d AS (
+    SELECT generate_series(_start_date::DATE, _end_date::DATE, '1 day')::DATE AS date
+  )
+  SELECT
+    COALESCE(d2.location_id, _location_id) AS location_id,
+    d.date::TEXT AS date,
+    COALESCE(d2.n, 0)::REAL AS n,
+    COALESCE(d2.n_count, 0)::REAL AS n_count,
+    COALESCE(d2.t, 0)::REAL AS t,
+    COALESCE(d2.sum_y, 0)::REAL AS sum_y,
+    COALESCE(d2.sum_t, 0)::REAL AS sum_t,
+    COALESCE(d2.y, 0)::REAL AS y,
+    COALESCE(d2.var_y, 0)::REAL AS var_y,
+    COALESCE(d2.df_num, 0)::REAL AS df_num,
+    COALESCE(d2.df_den, 0)::REAL AS df_den,
+    COALESCE(d2.se_y, 0)::REAL AS se_y,
+    COALESCE(d2.df, 0)::INT AS df
+  FROM d
+  LEFT JOIN d2 ON d.date = d2.date
+  ORDER BY d.date
+$$ LANGUAGE SQL;
+
 -- users stats and rank for given location, video start/end dates
 CREATE OR REPLACE FUNCTION f_users_stats(_location_id text, _start_date text, _end_date text)
 RETURNS TABLE (
